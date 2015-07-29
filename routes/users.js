561e5e127757c6cfbadb89992ab075fdd06a9c47
Eugene Skinner <eskinfrn@gmail.com>
2015-07-29 17:09:22 -0600

    protected user routes with the session

--- routes/users.js
+++ routes/users.js
@@ -37,6 +37,7 @@ router.post('/login',function(req,res,next){
 })
 
 router.get('/register',function(req,res,next){
+  req.session = null;
   res.render('register',{});
 })
 
@@ -74,52 +75,23 @@ router.get('/logout', function(req,res,next){
 });
 
 router.get('/:id/registerteams',function(req,res,next){
-  users.findOne({_id:req.params.id},function(err,file){
-    res.render('regteams',file)
-  })
+  if(req.session.email){
+    users.findOne({_id:req.params.id},function(err,file){
+      res.render('regteams',file)
+    })
+  }else{
+    res.redirect('/');
+  }
 });
 
 router.post('/:id/registerteams',function(req,res,next){
-  req.body.team_emails = req.body.team_emails.split(',');
-  teams.insert(req.body, function(err,team){
-    if(err){
-      return err
-    }else{
-      users.update({_id:req.params.id},{$push:{teams: team._id}},function(err,user){
-        if(err){
-          return err
-        }else{
-          res.redirect('/users/'+req.params.id)
-        }
-      })
-    }
-  })
-});
-
-router.get('/:id/new',function(req,res,next){
-  users.findOne({_id:req.params.id},function(err,user){
-    teams.find({_id: {$in: user.teams}}, function (err,teams){
-      user.teams = teams;
-      res.render('standupapp',user)
-    })
-  })
-})
-
-router.post('/:id/new',function(req,res,next){
-  console.log(req.body.teams)
-  teams.findOne({team_name: req.body.teams},function(err,team){
-    console.log(team)
-    transporter.sendMail({
-        from: 'standupapp@standup.com',
-        to: team.team_emails,
-        subject: req.body.name,
-        html: '<table><tr><th>Helps</th><th>Interesting</th><th>Events</th></tr><tr><td>'+req.body.help+'</td><td>'+req.body.interesting+'</td><td>'+req.body.events+'</td></tr></table>'
-    });
-    posts.insert({name: req.body.name, help: req.body.help, interesting: req.body.interesting, events: req.body.events}, function(err, post){
+  if(req.session.email){
+    req.body.team_emails = req.body.team_emails.split(',');
+    teams.insert(req.body, function(err,team){
       if(err){
         return err
       }else{
-        users.update({_id:req.params.id},{$push:{posts: post._id}},function(err,user){
+        users.update({_id:req.params.id},{$push:{teams: team._id}},function(err,user){
           if(err){
             return err
           }else{
@@ -128,19 +100,68 @@ router.post('/:id/new',function(req,res,next){
         })
       }
     })
-  })
+  }else{
+    res.redirect('/')
+  }
+});
+
+router.get('/:id/new',function(req,res,next){
+  if(req.session.email){
+    users.findOne({_id:req.params.id},function(err,user){
+      teams.find({_id: {$in: user.teams}}, function (err,teams){
+        user.teams = teams;
+        res.render('standupapp',user)
+      })
+    })
+  }else{
+    res.redirect('/')
+  }
+})
+
+router.post('/:id/new',function(req,res,next){
+  if(req.session.email){
+    console.log(req.body.teams)
+    teams.findOne({team_name: req.body.teams},function(err,team){
+      console.log(team)
+      transporter.sendMail({
+          from: 'standupapp@standup.com',
+          to: team.team_emails,
+          subject: req.body.name,
+          html: '<table><tr><th>Helps</th><th>Interesting</th><th>Events</th></tr><tr><td>'+req.body.help+'</td><td>'+req.body.interesting+'</td><td>'+req.body.events+'</td></tr></table>'
+      });
+      posts.insert({name: req.body.name, help: req.body.help, interesting: req.body.interesting, events: req.body.events}, function(err, post){
+        if(err){
+          return err
+        }else{
+          users.update({_id:req.params.id},{$push:{posts: post._id}},function(err,user){
+            if(err){
+              return err
+            }else{
+              res.redirect('/users/'+req.params.id)
+            }
+          })
+        }
+      })
+    })
+  }else{
+    res.redirect('/')
+  }
 });
 
 router.get('/:id',function(req,res,next){
-  users.findOne({_id: req.params.id}, function a(err,user){
-    teams.find({_id: {$in: user.teams}},function b(err,teams){
-      user.teams = teams;
-      posts.find({_id: {$in: user.posts}}, function c(err,posts){
-        user.posts = posts;
-        res.render('show', user)
+  if(req.session.email){
+    users.findOne({_id: req.params.id}, function a(err,user){
+      teams.find({_id: {$in: user.teams}},function b(err,teams){
+        user.teams = teams;
+        posts.find({_id: {$in: user.posts}}, function c(err,posts){
+          user.posts = posts;
+          res.render('show', user)
+        })
       })
     })
-  })
+  }else{
+    res.redirect('/')
+  }
 });
 
 
